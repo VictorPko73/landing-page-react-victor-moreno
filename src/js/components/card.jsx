2f71f7c82cc3ce77cdb51b6776a986cf67b92cbe
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-08-06 17:25:02 +0000

    Centrar titulo de Card

--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -5,7 +5,9 @@ const Card = ({ title, text, imageUrl, buttonText, onButtonClick }) => (
   <div className="card" style={{ width: "18rem" }}>
     <img src={imageUrl} className="card-img-top" alt={title} />
     <div className="card-body">
+      <div className="d-flex justify-content-center">
       <h5 className="card-title">{title}</h5>
+      </div>
       <p className="card-text">{text}</p>
       {buttonText && (
         <button className="btn btn-primary" onClick={onButtonClick}>
